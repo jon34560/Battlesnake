e612920176d789b7945624441b39ec557bec7a2d
automateddeploy <automateddeploy@redbrickmedia.com>
2017-03-03 16:47:27 -0800

    Update

--- silex/src/BattleSnake/Route/start.php
+++ silex/src/BattleSnake/Route/start.php
@@ -14,8 +14,6 @@ error_log(print_r($request_data, true), 0);
 $return_data = array(
   'color' => '#2222FF',
   'head_url' => 'https://cdn-images-1.medium.com/max/400/1*NOGCOyyj7wpAzkVGaLZqeA.gif',
-  'head_type' => 'tongue',
-  'tail_type' => 'skinny-tail',
   'name' => 'Sunglasses_And_Advil',
   'taunt' => 'Everyone wins!'
 );
