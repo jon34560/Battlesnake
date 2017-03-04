b1a247ef2507a832b5409615c2d1d707ac98d70c
automateddeploy <automateddeploy@redbrickmedia.com>
2017-03-04 12:00:17 -0800

    Some cleanup

--- silex/src/BattleSnake/Logic/FreeSpace/FreeSpace.php
+++ silex/src/BattleSnake/Logic/FreeSpace/FreeSpace.php
@@ -81,172 +81,166 @@ class FreeSpace
     }
 
 
-	/**
-	* floodFillDetection
-	*
-	* Description: check number of free spaces in each direction from current snake head.
-	*/
-	public static function floodFillDetection($state, $decision_matix) {
-		$fillWeight = 12; // 2; // 12 is better than 2
-		$my_snake = $state['snakes'][$state['s']];
+    /*
+    * floodFillDetection
+    *
+    * Description: check number of free spaces in each direction from current snake head.
+    */
+    public static function floodFillDetection($state, $decision_matix) {
+        $fillWeight = 12; // 2; // 12 is better than 2
+        $my_snake = $state['snakes'][$state['s']];
 
-		$leftSpaces = array();
-		$checkPosX = $my_snake['x'] - 1;
-		$checkPosY = $my_snake['y'];
-		$leftFill = self::floodFill( $state, $checkPosX, $checkPosY, $leftSpaces, $decision_matix );
-		$upSpaces = array();
-		$checkPosX = $my_snake['x'];
-		$checkPosY = $my_snake['y'] - 1;
-		$upFill = self::floodFill( $state, $checkPosX, $checkPosY, $upSpaces, $decision_matix );
-		$rightSpaces = array();
-		$checkPosX = $my_snake['x'] + 1;
-		$checkPosY = $my_snake['y'];
-		$rightFill = self::floodFill( $state, $checkPosX, $checkPosY, $rightSpaces, $decision_matix );
-		$downSpaces = array();
-		$checkPosX = $my_snake['x'];
-		$checkPosY = $my_snake['y'] + 1;
-		$downFill = self::floodFill( $state, $checkPosX, $checkPosY, $downSpaces, $decision_matix );
-		
-		$avoidLeft = false;
-		$avoidUp = false;
-		$avoidRight = false;
-		$avoidDown = false;
+        $leftSpaces = [];
+        $checkPosX = $my_snake['x'] - 1;
+        $checkPosY = $my_snake['y'];
+        $leftFill = self::floodFill($state, $checkPosX, $checkPosY, $leftSpaces, $decision_matix);
+        $avoidLeft = false;
 
-		$snakeLength = count($my_snake['tails']) + 1;
-		if( $leftFill > 0 && $leftFill <= count($my_snake['tails']) * 2 ){ // Is there enough space to the left to fit the snake.
-			$avoidLeft = true;
-		}
-		if( $upFill > 0 && $upFill <= count($my_snake['tails']) * 2 ){
-			$avoidUp = true;
-		} 
-		if( $rightFill > 0 && $rightFill <= count($my_snake['tails']) * 2 ){
-			$avoidRight = true;
-		}
-		if( $downFill > 0 && $downFill <= count($my_snake['tails']) * 2 ){
-			$avoidDown = true;
-		}
+        $rightSpaces = [];
+        $checkPosX = $my_snake['x'] + 1;
+        $checkPosY = $my_snake['y'];
+        $rightFill = self::floodFill($state, $checkPosX, $checkPosY, $rightSpaces, $decision_matix);
+        $avoidRight = false;
 
-		// If one direction is good and one or more other directions are bad, increase the priority.
-		if ($decision_matix->getAllowedDirectionValue('left') && ($leftFill > $snakeLength*2 && ( $avoidUp || $avoidRight || $avoidDown )) ){
-      		$decision_matix->incrementPreferedDirectionValue('left', 50);
-			//$targetLeft += 50;
-		}
-		if ($decision_matix->getAllowedDirectionValue('up') && ($upFill > $snakeLength*2 && ( $avoidLeft || $avoidRight || $avoidDown )) ){
-      		$decision_matix->incrementPreferedDirectionValue('up', 50);
-			//$targetUp += 50;
-		}
-		if ($decision_matix->getAllowedDirectionValue('right') && ($rightFill > $snakeLength*2 && ( $avoidLeft || $avoidUp || $avoidDown )) ){
-      		$decision_matix->incrementPreferedDirectionValue('right', 50);
-      		//$targetRight += 50;
-		}
-		if ($decision_matix->getAllowedDirectionValue('down') && ($downFill > $snakeLength*2 && ( $avoidLeft || $avoidUp || $avoidRight )) ){
-      		$decision_matix->incrementPreferedDirectionValue('down', 50);
-      		//$targetDown += 50;
-		}
-		
-		// Chose direction based on direction counts found.
-		$directions = array( 'left' => $leftFill, 'up' => $upFill, 'right' => $rightFill, 'down' => $downFill );
-		arsort($directions);
-		reset($directions);
-		$bestKey = key($directions);
-		$bestValue = $directions[$bestKey];
-		asort($directions);
-		reset($directions);
-		$worstKey = key($directions);
-		$worstValue = $directions[$worstKey];
-		//echo " best " . $bestKey . " v " . $bestValue . "   ----- worst " . $worstKey. " v " .$worstValue. "<br>"; 
-        if($bestValue > 0 && $bestValue > $worstValue){
-			if($bestKey == 'left' && $decision_matix->getAllowedDirectionValue('left')){
-				//$targetLeft += $fillWeight;
-				$decision_matix->incrementPreferedDirectionValue('left', $fillWeight);
-			}
-			if($bestKey == 'up' && $decision_matix->getAllowedDirectionValue('up')){
-				//$targetUp += $fillWeight;
-				$decision_matix->incrementPreferedDirectionValue('up', $fillWeight);
-			}
-			if($bestKey == 'right' && $decision_matix->getAllowedDirectionValue('right')){
-				//$targetRight += $fillWeight;
-				$decision_matix->incrementPreferedDirectionValue('right', $fillWeight);
-			}
-			if($bestKey == 'down' && $decision_matix->getAllowedDirectionValue('down')){
-				//$targetDown += $fillWeight;
-				$decision_matix->incrementPreferedDirectionValue('down', $fillWeight);
-			}
+        $upSpaces = [];
+        $checkPosX = $my_snake['x'];
+        $checkPosY = $my_snake['y'] - 1;
+        $upFill = self::floodFill($state, $checkPosX, $checkPosY, $upSpaces, $decision_matix);
+        $avoidUp = false;
+
+        $downSpaces = [];
+        $checkPosX = $my_snake['x'];
+        $checkPosY = $my_snake['y'] + 1;
+        $downFill = self::floodFill($state, $checkPosX, $checkPosY, $downSpaces, $decision_matix);
+        $avoidDown = false;
+
+        if ($leftFill > 0 && $leftFill <= count($my_snake['tails']) * 2) { // Is there enough space to the left to fit the snake.
+            $avoidLeft = true;
         }
-	}
+        if ($upFill > 0 && $upFill <= count($my_snake['tails']) * 2) {
+            $avoidUp = true;
+        }
+        if ($rightFill > 0 && $rightFill <= count($my_snake['tails']) * 2) {
+            $avoidRight = true;
+        }
+        if ($downFill > 0 && $downFill <= count($my_snake['tails']) * 2) {
+            $avoidDown = true;
+        }
+
+        // If one direction is good and one or more other directions are bad, increase the priority.
+        if ($decision_matix->getAllowedDirectionValue('left') && ($leftFill > $my_snake['length']*2 && ( $avoidUp || $avoidRight || $avoidDown ))) {
+              $decision_matix->incrementPreferedDirectionValue('left', 50);
+        }
+        if ($decision_matix->getAllowedDirectionValue('up') && ($upFill > $my_snake['length']*2 && ( $avoidLeft || $avoidRight || $avoidDown ))) {
+              $decision_matix->incrementPreferedDirectionValue('up', 50);
+        }
+        if ($decision_matix->getAllowedDirectionValue('right') && ($rightFill > $my_snake['length']*2 && ( $avoidLeft || $avoidUp || $avoidDown ))) {
+              $decision_matix->incrementPreferedDirectionValue('right', 50);
+        }
+        if ($decision_matix->getAllowedDirectionValue('down') && ($downFill > $my_snake['length']*2 && ( $avoidLeft || $avoidUp || $avoidRight ))) {
+              $decision_matix->incrementPreferedDirectionValue('down', 50);
+        }
+
+        // Chose direction based on direction counts found.
+        $directions = ['left' => $leftFill, 'up' => $upFill, 'right' => $rightFill, 'down' => $downFill];
+        arsort($directions);
+        reset($directions);
+        $bestKey = key($directions);
+        $bestValue = $directions[$bestKey];
+        asort($directions);
+        reset($directions);
+        $worstKey = key($directions);
+        $worstValue = $directions[$worstKey];
+
+        if ($bestValue > 0 && $bestValue > $worstValue) {
+            if ($bestKey == 'left' && $decision_matix->getAllowedDirectionValue('left')) {
+                $decision_matix->incrementPreferedDirectionValue('left', $fillWeight);
+            }
+            if ($bestKey == 'right' && $decision_matix->getAllowedDirectionValue('right')) {
+                $decision_matix->incrementPreferedDirectionValue('right', $fillWeight);
+            }
+            if ($bestKey == 'up' && $decision_matix->getAllowedDirectionValue('up')) {
+                $decision_matix->incrementPreferedDirectionValue('up', $fillWeight);
+            }
+            if ($bestKey == 'down' && $decision_matix->getAllowedDirectionValue('down')) {
+                $decision_matix->incrementPreferedDirectionValue('down', $fillWeight);
+            }
+        }
+    }
 
 
-	/**
-	* floodFill
-	*
-	* Description: Check tiles in one direction to see how many open are connected.
-	*
-	* Optimization: Don't search farther than we have to... 
-	*/
-	public static function floodFill( $state, $checkPosX, $checkPosY, &$spaces, $decision_matix, $depth = 0 ){
-		if($depth > 20){
-			return 0;
-		}
-		$oldCheckPosX = $checkPosX;
-		$oldCheckPosY = $checkPosY;
-		$fillCount = 0;
-		$key = $checkPosX . '_' . $checkPosY;
+    /*
+    * floodFill
+    *
+    * Description: Check tiles in one direction to see how many open are connected.
+    *
+    * Optimization: Don't search farther than we have to...
+    */
+    public static function floodFill($state, $checkPosX, $checkPosY, &$spaces, $decision_matix, $depth = 0) {
+        // Limit floodFill recursion
+        if ($depth > 200) {
+            return 0;
+        }
 
-		if(array_key_exists($key, $spaces)){
-			return 0;
-		}
-		$isEmpty = false;
-		if( Board::isSpaceEmpty($state, $checkPosX, $checkPosY, $decision_matix) ){
-			$fillCount++;
-			$isEmpty = true;
-		}
-		$spaces[$key] = array('open' => $isEmpty, 'x' => $checkPosX, 'y' => $checkPosY);
-		//echo " scan ". $key. "  o: $isEmpty    d $depth    <br>";	
-		// Find next pos to scan 
-		$i = 0;
-		//echo " . " . "  " . $i . " : " . count($spaces ) . "<br>";
-		if( $spaces[$key]['open'] == true ){
-			// Left
-			$tx = $spaces[$key]['x'] - 1;
-			$ty = $spaces[$key]['y'];
-			$tKey = $tx . '_' . $ty; 
-			if( Board::isSpaceOnBoard( $state, $tx , $ty, $decision_matix) && !array_key_exists($tKey, $spaces) ){
-				$checkPosX = $tx;
-				$checkPosY = $ty;
-				$fillCount += self::floodFill( $state, $checkPosX, $checkPosY, $spaces, $decision_matix, $depth+1 );
-			}
-			
-			// Up
-			$tx = $spaces[$key]['x'];
-			$ty = $spaces[$key]['y'] - 1;  
-			$tKey = $tx . '_' . $ty;              
-			if( Board::isSpaceOnBoard( $state, $tx , $ty, $decision_matix) && !array_key_exists($tKey, $spaces) ){
-				$checkPosX = $tx;
-				$checkPosY = $ty;
-				$fillCount += self::floodFill( $state, $checkPosX, $checkPosY, $spaces, $decision_matix, $depth+1 );
-			}		 
-				
-			// Right
-			$tx = $spaces[$key]['x'] + 1;
-			$ty = $spaces[$key]['y'];
-			$tKey = $tx . '_' . $ty;
-			if( Board::isSpaceOnBoard( $state, $tx , $ty, $decision_matix) && !array_key_exists($tKey, $spaces) ){
-				$checkPosX = $tx;
-				$checkPosY = $ty;
-				$fillCount += self::floodFill( $state, $checkPosX, $checkPosY, $spaces, $decision_matix, $depth+1 );
-			}
-			
-			// Down
-			$tx = $spaces[$key]['x'];
-			$ty = $spaces[$key]['y'] + 1;
-			$tKey = $tx . '_' . $ty;
-			if( Board::isSpaceOnBoard( $state, $tx , $ty, $decision_matix) && !array_key_exists($tKey, $spaces) ){
-				$checkPosX = $tx;
-				$checkPosY = $ty;
-				$fillCount += self::floodFill( $state, $checkPosX, $checkPosY, $spaces, $decision_matix, $depth+1 );
-			}		
-		}
-		return $fillCount;
-	}
+        $oldCheckPosX = $checkPosX;
+        $oldCheckPosY = $checkPosY;
+        $fillCount = 0;
+        $key = $checkPosX . '_' . $checkPosY;
 
+        if (array_key_exists($key, $spaces)) {
+            return 0;
+        }
+
+        $isEmpty = false;
+        if (Board::isSpaceEmpty($state, $checkPosX, $checkPosY, $decision_matix)) {
+            $fillCount++;
+            $isEmpty = true;
+        }
+
+        $spaces[$key] = ['open' => $isEmpty, 'x' => $checkPosX, 'y' => $checkPosY];
+        // Find next pos to scan
+        $i = 0;
+        if ($spaces[$key]['open'] == true) {
+            // Left
+            $tx = $spaces[$key]['x'] - 1;
+            $ty = $spaces[$key]['y'];
+            $tKey = $tx . '_' . $ty;
+            if (Board::isSpaceOnBoard($state, $tx, $ty, $decision_matix) && !array_key_exists($tKey, $spaces)) {
+                $checkPosX = $tx;
+                $checkPosY = $ty;
+                $fillCount += self::floodFill($state, $checkPosX, $checkPosY, $spaces, $decision_matix, $depth+1);
+            }
+
+            // Up
+            $tx = $spaces[$key]['x'];
+            $ty = $spaces[$key]['y'] - 1;
+            $tKey = $tx . '_' . $ty;
+            if (Board::isSpaceOnBoard($state, $tx , $ty, $decision_matix) && !array_key_exists($tKey, $spaces)) {
+                $checkPosX = $tx;
+                $checkPosY = $ty;
+                $fillCount += self::floodFill($state, $checkPosX, $checkPosY, $spaces, $decision_matix, $depth+1);
+            }
+
+            // Right
+            $tx = $spaces[$key]['x'] + 1;
+            $ty = $spaces[$key]['y'];
+            $tKey = $tx . '_' . $ty;
+            if (Board::isSpaceOnBoard($state, $tx , $ty, $decision_matix) && !array_key_exists($tKey, $spaces)) {
+                $checkPosX = $tx;
+                $checkPosY = $ty;
+                $fillCount += self::floodFill($state, $checkPosX, $checkPosY, $spaces, $decision_matix, $depth+1);
+            }
+
+            // Down
+            $tx = $spaces[$key]['x'];
+            $ty = $spaces[$key]['y'] + 1;
+            $tKey = $tx . '_' . $ty;
+            if (Board::isSpaceOnBoard($state, $tx , $ty, $decision_matix) && !array_key_exists($tKey, $spaces)) {
+                $checkPosX = $tx;
+                $checkPosY = $ty;
+                $fillCount += self::floodFill($state, $checkPosX, $checkPosY, $spaces, $decision_matix, $depth+1);
+            }
+        }
+        return $fillCount;
+    }
 }
